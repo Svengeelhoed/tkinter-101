3cd6a9de298a97b4da9c80599bbe5151675106b3
Svengeelhoed <99069588@mydavinci.nl>
2022-02-23 09:58:02 +0100

    klok gemaakt

---
+++ klok.py
@@ -0,0 +1,20 @@
+import tkinter
+
+from time import strftime
+ 
+root = tkinter.Tk()
+root.title('Clock')
+ 
+def time():
+    string = strftime('%H:%M:%S')
+    lbl.config(text = string)
+    lbl.after(1000, time)
+ 
+lbl = tkinter.Label(root, font = ('comicsans', 40, 'bold'),
+            background = 'lightblue',
+            foreground = 'black')
+
+lbl.pack(anchor = 'center')
+time()
+ 
+root.mainloop()
